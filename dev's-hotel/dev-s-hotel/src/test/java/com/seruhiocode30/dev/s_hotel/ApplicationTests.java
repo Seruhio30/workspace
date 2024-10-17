a42bb15fe5c9c406d1d0f19246ec0171268814c4
Sergio Herrera Rivera <sh0041314@gmail.com>
2024-10-17 10:12:04 -0600

    trabajo incompleto dev hotel

---
+++ dev's-hotel/dev-s-hotel/src/test/java/com/seruhiocode30/dev/s_hotel/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seruhiocode30.dev.s_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
